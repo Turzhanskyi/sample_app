463d2a2b1912e10f99c287e9636d83d59fad023a
Vitalii <turzhansky81@gmail.com>
2020-11-25 11:48:32 +0200

    Add interface for manipulating microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def home; end
+  def home
+    @micropost  = current_user.microposts.build if logged_in?
+    @feed_items = current_user.feed.paginate(page: params[:page]) if logged_in?
+  end
 
   def help; end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,13 +58,6 @@ class UsersController < ApplicationController
 
   # Before filters
 
-  # Confirms a logged-in user.
-  def logged_in_user
-    store_location unless logged_in?
-    flash[:danger] = 'Please log in.' unless logged_in?
-    redirect_to login_url unless logged_in?
-  end
-
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
